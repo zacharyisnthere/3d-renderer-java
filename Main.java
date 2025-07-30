8d28a14aacf252ea6b2034ba76ec7aa189032ca3
zacharyisnthere <zachbrian.petersen@gmail.com>
2025-07-30 10:27:29 -0600

    Make toCameraSpace in camera its own method

--- Main.java
+++ Main.java
@@ -282,37 +282,38 @@ class Camera {
 
 
     public Point2D project(Point3D point, int screenWidth, int screenHeight) {
-        //translate point relative to camera
-        double relX = point.x - pos.x;
-        double relY = point.y - pos.y;
-        double relZ = point.z - pos.z;
+        Point3D camSpace = toCameraSpace(point);
 
-        //apply yaw rotation (global y axis)
-        double cosYaw = Math.cos(yaw), sinYaw = Math.sin(yaw);
-        double rotX = cosYaw * relX - sinYaw * relZ;
-        double rotZ1 = sinYaw * relX + cosYaw * relZ;
-
-        //apply pitch rotatoin (local x axis)
-        double cosPitch = Math.cos(pitch), sinPitch = Math.sin(pitch);
-        double rotY = cosPitch * relY - sinPitch * rotZ1;
-        double rotZ = sinPitch * relY + cosPitch * rotZ1;
-
-        if (rotZ<=clipNear) return null;
-        if (rotZ>=clipFar) return null;
-
-        // projection math and stuff. screenY is flipped because 0,0 is top left which is odd.
-        // f is the vertical fov, so when the screen isn't square the horizontal scaling is weird.
-        // by dividing f by the ratio of screen width/height, the screenX value is corrected.
+        if (camSpace.z <= clipNear || camSpace.z >= clipFar) return null;
 
         double aspect = (double) screenWidth / screenHeight;
         double f = 1.0 / Math.tan(Math.toRadians(fov) / 2);
         
-        double screenX = (rotX / rotZ) * f/aspect * screenWidth/2 + screenWidth/2;
-        double screenY = (-rotY / rotZ) * f * screenHeight/2 + screenHeight/2;
+        double screenX = (camSpace.x / camSpace.z) * f/aspect * screenWidth/2 + screenWidth/2;
+        double screenY = (-camSpace.y / camSpace.z) * f * screenHeight/2 + screenHeight/2;
 
         return new Point2D.Double(screenX, screenY);
     }
-    
+
+    public Point3D toCameraSpace(Point3D world) {
+        double relX = world.x - pos.x;
+        double relY = world.y - pos.y;
+        double relZ = world.z - pos.z;
+
+        double cosYaw = Math.cos(yaw), sinYaw = Math.sin(yaw);
+        double rotX = cosYaw * relX - sinYaw * relZ;
+        double rotZ1 = sinYaw * relX + cosYaw * relZ;
+
+        double cosPitch = Math.cos(pitch), sinPitch = Math.sin(pitch);
+        double rotY = cosPitch * relY - sinPitch * rotZ1;
+        double rotZ = sinPitch * relY + cosPitch * rotZ1;
+
+        return new Point3D(rotX, rotY, rotZ);
+
+        // projection math and stuff. screenY is flipped because 0,0 is top left which is odd.
+        // f is the vertical fov, so when the screen isn't square the horizontal scaling is weird.
+        // by dividing f by the ratio of screen width/height, the screenX value is corrected.
+    }    
 }
 
 
